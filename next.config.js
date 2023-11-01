4f8adb19f28cea7f411d4407174b2eda9890397c
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-01 09:15:42 +0600

    prisma adapter

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["google.com", "avatars.githubusercontent.com"],
+    domains: [
+      "google.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
